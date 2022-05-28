714403b863261240b34855f7a84030fffa1677a3
Aleksei Gutikov <gutikoff@gmail.com>
2022-05-28 11:07:39 +0300

    xxx

--- test_em_gmm_1d.py
+++ test_em_gmm_1d.py
@@ -5,16 +5,22 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-data = np.ones(1)*100
+#data = np.ones(1)*400
+data = np.empty(1)
 
-for i in range(2):
-    data = np.concatenate((data, np.random.normal(i*10+10, 2*i+1, 100)))
+for i in range(3):
+    data = np.concatenate((data, np.random.normal(i*10+100, 20*i+1, 4)))
 
 
 
 
-mu, sigma = em_gmm_1d.em_gmm_1d(data, 10, False)
+mu, sigma = em_gmm_1d.em_gmm_1d(data, 1, True)
 
 print(mu, sigma)
 
-em_gmm_1d.plot_1d(data, mu, sigma, 'black')
+em_gmm_1d.plot_1d(data, mu, sigma, 'y')
+
+m = np.median(data)
+s = np.std(data)
+print(m, s)
+em_gmm_1d.plot_1d(data, [m], [s], 'b')
